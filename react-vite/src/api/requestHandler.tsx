c37201ea67050d78e5d588e07859e556289396cf
Thompson Doan <thompson.doanthai@loricahealth.com>
2024-06-29 17:16:43 +1000

    Updated to use php laravel backend instead of a lambda

--- react-vite/src/api/requestHandler.tsx
+++ react-vite/src/api/requestHandler.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const requestHandler = axios.create({
-  baseURL: "https://9k1hg47b1e.execute-api.ap-southeast-2.amazonaws.com/prd",
   timeout: 30000,
   headers: {},
 });
