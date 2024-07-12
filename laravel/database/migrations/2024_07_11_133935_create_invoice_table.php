a1bc563db948a35c24189c2706a89efb84373035
Thompson Doan <thompson.doanthai@loricahealth.com>
2024-07-12 10:15:01 +1000

    Added Invoice database table

---
+++ laravel/database/migrations/2024_07_11_133935_create_invoice_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoice', function (Blueprint $table) {
+            $table->id();
+            $table->text('abn');
+            $table->text('contact');
+            $table->text('customerID');
+            $table->date('date');
+            $table->text('email');
+            $table->text('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoice');
+    }
+};
